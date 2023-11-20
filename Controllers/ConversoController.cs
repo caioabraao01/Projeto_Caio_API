01e9c43406fc258c1fe74524a2fb984815842388
Caio Abraão Oliveira Rocha <69085107+caioabraao01@users.noreply.github.com>
2023-11-20 01:38:47 -0300

    Projeto_API

---
+++ Controllers/ConversoController.cs
@@ -0,0 +1,56 @@
+﻿using Caio_Teste.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caio_Teste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult converse(float? Anos_Luz, float? km)
+        {
+
+            var message = new Message();
+
+            var isKmValid = km >= 1;
+            var isAnosLuzValid = Anos_Luz >= 1;
+
+            // Calcular Km -> Anos Luz
+            if(isKmValid)
+            {
+                var _anosLuz = km * 1.057 * Math.Pow(10, -13);
+
+                message.value = (float)_anosLuz;
+                message.statusCode = 200;
+                message.dataTime = DateTime.Now;
+                return Ok(message);
+            }
+
+            // Calcular Anos Luz -> Km
+            if (isAnosLuzValid)
+            {
+                var _km = Anos_Luz * 9.46 * Math.Pow(10,12);
+
+                message.value = (float)_km;
+                message.statusCode = 200;
+                message.dataTime = DateTime.Now;
+                return Ok(message);
+            }
+
+            message.errorMessage = "invalid value";
+            message.statusCode = 400;
+            message.dataTime = DateTime.Now;
+
+            return BadRequest(message);
+
+        }
+
+        //[HttpGet]
+        //public IActionResult converseKm(float km)
+        //{
+
+        //}
+    }
+}
---
+++ Model/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Caio_Teste.Model
+{
+    public class Message
+    {
+        public int statusCode { get; set; }
+        public string? errorMessage { get; set; }
+        public DateTime dataTime { get; set; }
+        public float value { get; set; }
+    }
+}
